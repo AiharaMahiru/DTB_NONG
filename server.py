722e44a41954cce2f78a6f35350203b7617ce948
AiharaMahiru <vni@outlook.jp>
2023-04-19 10:51:02 +0800

    change

--- server.py
+++ server.py
@@ -31,23 +31,22 @@ if count == 0:
     cur.execute(f"create table pesticide ({columns})")
     conn.commit()
 
-# 执行插入操作 placeholders = str
-placeholders = ", ".join(["%s"] * len(json_data))
-values = list(json_data.values())
-# <class 'list'>
+# 执行插入操作
+placeholders: str = ", ".join(["%s"] * len(json_data))
+values: list = list(json_data.values())
 # [{'name': '敌敌畏', 'type': '杀虫剂', 'price': 10, 'count': 100, 'inventory': 100, 'info': {'用途': '杀虫', '用量': '1ml/平方米', '有效期': '1年'}}]
 
 # 构造列名
-# columns = ", ".join(values[0].keys())
+columns = ", ".join(values[0].keys())
 
 # # 构造值
-# values_str = ", ".join(["(%s)" % ", ".join(["%s"] * len(values[0]))] * len(values))
+values_str: str = ", ".join(["(%s)" % ", ".join(["%s"] * len(values[0]))] * len(values))
 
-# # 构造 SQL 语句
-# sql = "insert into pesticide (%s) values %s" % (columns, values_str)
+# # 构造 SQL 语句 insert into pesticide (name, type, price, count, inventory, info) values (%s, %s, %s, %s, %s, %s)
+sql: str = "insert into pesticide (%s) values %s" % (columns, values_str)
 
 # # 执行 SQL 语句
-# cur.execute(sql, [v for d in values for v in d.values()])
+cur.execute(sql, [v for d in values for v in d.values()])
 # conn.commit()
 cur.close()
 conn.close()
